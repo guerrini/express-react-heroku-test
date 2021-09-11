ebedac0f9687d7dd3167b3ba1c0648c83b9550a3
Steven Guerrini <sguerrini@gmail.com>
2021-09-11 11:47:46 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,28 @@
+// var http = require("http");
+
+// //create a server object:
+// http
+//   .createServer(function(req, res) {
+//     res.write("Hello World!"); //write a response to the client
+//     res.end(); //end the response
+//   })
+//   .listen(8080); //the server object listens on port 8080
+
+// const ShoppingList = require("./components/ShoppingList.js");
+const express = require("express");
+const app = express();
+const port = 8080;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/foo", (req, res) => {
+  res.send("Foo!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
+
+//<ShoppingList name="Mark" />
